2c0b9bcbf0121f6aae1f08482fa53dd699fb1739
Muritala  Aminu <muritala.aminu@softrams.com>
2021-10-20 13:32:55 -0500

    feat: test

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "b" {
-  #ts:skip=AC_AWS_0207
-  #ts:skip=AC_AWS_0214
-  #ts:skip=AWS.AKK.DP.HIGH.0012
-  #ts:skip=AC_AWS_0497
+ #ts:skip=AC_AWS_0207
+ #ts:skip=AC_AWS_0214
+ #ts:skip=AWS.AKK.DP.HIGH.0012
+ #ts:skip=AC_AWS_0497
+
 
   bucket = "ami-tf-test-bucket"
   acl    = "private"
